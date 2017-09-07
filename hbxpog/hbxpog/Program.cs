1e8079ccc098fa4da9499d8273ad54e27a94fefc
TJ Wolschon <daxtron2@users.noreply.github.com>
2017-09-07 14:16:42 -0400

    Update Program.cs

--- hbxpog/hbxpog/Program.cs
+++ hbxpog/hbxpog/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 namespace hbxpog
 {
     /* hbxpog (enigma encoded with 3/14/15/9/2/6, ie Pi)
-     * Inspired by the Nazi's Enigma machine and One Time Pad ciphers
+     * Inspired by the Germany's Enigma machine, before and during WW2, and One Time Pad ciphers
      * Created by TJ Wolschon 4/4/2017
      * This program creates One Time Pads for use in encoding and decoding messages.
      * The program can also do the encoding and decoding of the messages if the user chooses.
